c4fb8703689e8788e85a69c6645375c285e44004
PetersonE1 <PetersonE_1@live.com>
2024-02-05 18:45:40 -0500

    Add files via upload

--- test_runner_1.py
+++ test_runner_1.py
@@ -3,6 +3,6 @@ import File1 as test
 cases = [["1 + 1", "2"], ["1 + 2", "3"], ["1 - 1", "0"]]
 
 for case in cases:
-    assert test.run(case[0]) == case[1]
+    assert str(test.run(case[0])) == case[1]
 
 print("OK")
