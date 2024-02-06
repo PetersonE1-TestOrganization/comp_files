e543f51cecb7db14670dd6da4543649b3b28a8e4
PetersonE1 <PetersonE_1@live.com>
2024-02-06 17:51:15 -0500

    Update test_runner_1.py

--- test_runner_1.py
+++ test_runner_1.py
@@ -1,8 +1,19 @@
-import File1 as test
+# Tests the four mathematical operators
 
-cases = [["1 + 1", "2"], ["1 + 2", "3"], ["1 - 1", "0"]]
+import File1 as test_module
+
+cases = [
+    ["537 + 264", "801"],
+    ["5278 + 1469", "6747"],
+    ["396 - 195", "201"],
+    ["2497 - 8265", "-5768"],
+    ["385 * 105", "40425"],
+    ["8562 * 1356", "11610072"],
+    ["512 / 16", "32"],
+    ["100016 / 658", "152"]
+]
 
 for case in cases:
-    assert str(test.run(case[0])) == case[1]
+    assert str(test_module.run(case[0])) == case[1]
 
 print("OK")
